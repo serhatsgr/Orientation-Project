4308de7e345d393e4d6fbdf86093032546b17661
Serhat Sağır <95631951+serhatsgr@users.noreply.github.com>
2024-04-01 00:58:10 +0300

    egzersiz

---
+++ Workouts/App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
+
+export default function App() {
+  const exercise = exercises[2];
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.exerciseContainer}> 
+        <Text style= {styles.exerciseName}>{exercise.name}</Text>
+        <Text style= {styles.exerciseSubtitle}>{exercise.muscle.toUpperCase()} I {exercise.equipment.toUpperCase()}</Text>
+      </View>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gainsboro',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  exerciseContainer:{
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize:20,
+    fontWeight:"500",
+  },
+  exerciseSubtitle: {
+    color:"dimgray"
+  }
+});
